d69e993343d6a5435d9ba8f4e0f8b8f44826fc4b
Vitor Costa <vitortecnologia62@gmail.com>
2023-03-19 02:46:56 -0300

    Cria logo de inicialização

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require('express');
+const logo = require('./assets/js/logo');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('assets'));
 
+var port = 8080;
+
 app.get("/", (req, res) =>{
-    res.render("index")
+    res.render("index");
 });
 
 app.get("/comunidade", (req, res) =>{
-    res.render("community")
+    res.render("community");
 });
 
 
-app.listen(8080, ()=> {console.log("App Rodando!")})
+app.listen(port, () => {
+
+    console.log("Sua aplicação foi iniciada com sucesso!");
+    console.log("");
+    console.log("Acesse em: http://localhost:" + port + "/");
+    console.log("");
+
+});
+
+/*
+
+*/
