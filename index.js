a459b79f0499771be74945d390b4c485cf0755f8
Vitor Costa <vitortecnologia62@gmail.com>
2023-03-18 21:24:21 -0300

    Adiciona arquivos staticos e rotas

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static('assets'));
 
 app.get("/", (req, res) =>{
     res.render("index")
